4c5e3a39d63feaa573392f987921a50f39c0c326
Mike Perrotta <michaelvperrotta@gmail.com>
2021-01-28 19:36:57 -0800

    Track pull ups

--- PullUpPi.py
+++ PullUpPi.py
@@ -8,7 +8,7 @@ camera.resolution = (320, 240)
 camera.hflip = True
 output = np.empty((camera.resolution[1], camera.resolution[0], 3), dtype=np.uint8)
 
-camera.start_preview(alpha=200)
+camera.start_preview(alpha=235)
 
 def draw_square(corners, color, layer=3):
     top, right, bottom, left = corners
@@ -25,8 +25,8 @@ def draw_square(corners, color, layer=3):
     frame[top:bottom, right] = color
     return camera.add_overlay(np.asarray(frame), layer=layer)
 
-pullUpAreaTop = camera.resolution[1] * 1 // 3
-pullUpAreaBottom = camera.resolution[1] * 2 // 3
+pullUpAreaTop = camera.resolution[1] * 2 // 5
+pullUpAreaBottom = camera.resolution[1] * 3 // 5
 
 draw_square((pullUpAreaTop, camera.resolution[0] - 1, pullUpAreaBottom, 0), (120, 120, 120, 255))
 
@@ -34,25 +34,49 @@ overlay = None
 
 last_capture_time = time.time()
 
+pull_ups = 0
+is_descending = True  # True if face has completed a pull up but hasn't yet lowered fully
+
+validTop, validBottom, validLeft, validRight = (40, 200, 80, 240)
+draw_square((validTop, validRight, validBottom, validLeft), (0, 0, 0, 20))
+
 while True:
-    print("Capturing image. ({}s since last capture)".format(time.time() - last_capture_time))
+    print("Capturing image. ({:.4}s since last capture)".format(time.time() - last_capture_time))
     last_capture_time = time.time()
 
-    camera.capture(output, format="rgb", use_video_port=True)
-
-    face_locations = face_recognition.face_locations(output)
-    print("Found {} faces in image.".format(len(face_locations)))
+    camera.annotate_text = "Pull ups: {}".format(pull_ups)
     
-    for o in camera.overlays[1:]:
+    camera.capture(output, format="rgb", use_video_port=True)
+    print("Capture took {}s".format(time.time() - last_capture_time))
+
+    face_locations = face_recognition.face_locations(output[validTop:validBottom, validLeft:validRight, :])
+    print("Found {} faces in image.".format(len(face_locations)))
+        
+    for o in camera.overlays[2:]:
         camera.remove_overlay(o)
     
     for l in face_locations:
-        top = l[0]
-        print(top)
-        color = (255, 0, 0, 200)
-        if top < pullUpAreaBottom:
-            color = (0, 13, 255, 200)
-        if top < pullUpAreaTop:
-            color = (0, 207, 17, 200)
-        draw_square(l, color)
+        # first, adjust locations to be relative to our screen instead of our valid area
+        l = list(l)
+        l[0] += validTop
+        l[1] += validLeft
+        l[2] += validTop
+        l[3] += validLeft
         
+        top = l[0]
+        
+        if top > pullUpAreaBottom:
+            # face is fully lowered
+            color = (255, 0, 0, 200)
+            is_descending = False
+        elif top > pullUpAreaTop:
+            # face is between thresholds
+            color = (0, 13, 255, 200)
+        else:
+            # face is fully up
+            color = (0, 207, 17, 200)
+            if not is_descending:
+                pull_ups += 1
+                is_descending = True
+        
+        draw_square(l, color)
